01bb569ac78ef570b9a3ff9dec2b4e2d9b6b191c
ERIK RICARDO BERGANTIN BALTHAZAR <1510031171@acad.dvuadmin.net>
2018-03-28 20:54:48 -0300

    cadastro medico - fluxo sem validacao

--- app/src/main/java/br/com/tcc/prontuario/prontuario/MedSignupPasswordActivity.java
+++ app/src/main/java/br/com/tcc/prontuario/prontuario/MedSignupPasswordActivity.java
@@ -1,7 +1,10 @@
 package br.com.tcc.prontuario.prontuario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MedSignupPasswordActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MedSignupPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_med_signup_password);
+
+        Button nextButton = findViewById(R.id.finish_signup_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextScreen();
+            }
+        });
+    }
+
+    private void nextScreen() {
+        /**
+         *  Criar a tela de DASHBOARD
+         */
+        Intent intent = new Intent(MedSignupPasswordActivity.this,
+                LoginActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/br/com/tcc/prontuario/prontuario/MedSignupPersonalDataActivity.java
+++ app/src/main/java/br/com/tcc/prontuario/prontuario/MedSignupPersonalDataActivity.java
@@ -1,7 +1,10 @@
 package br.com.tcc.prontuario.prontuario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MedSignupPersonalDataActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MedSignupPersonalDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_med_signup_personal_data);
+
+        Button nextButton = findViewById(R.id.personal_data_next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextScreen();
+            }
+        });
+    }
+
+    private void nextScreen() {
+        Intent intent = new Intent(MedSignupPersonalDataActivity.this,
+                MedSignupPasswordActivity.class);
+        startActivity(intent);
     }
 }
