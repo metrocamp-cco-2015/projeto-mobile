fdbb9cadafc612ac506392309943a339de798145
ERIK RICARDO BERGANTIN BALTHAZAR <1510031171@acad.dvuadmin.net>
2018-03-28 20:22:21 -0300

    cadastro medico - dados de contato e pessoais

---
+++ app/src/main/java/br/com/tcc/prontuario/prontuario/MedSignupContact.java
@@ -0,0 +1,13 @@
+package br.com.tcc.prontuario.prontuario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MedSignupContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_med_signup_contact);
+    }
+}
---
+++ app/src/main/java/br/com/tcc/prontuario/prontuario/MedSignupPersonalData.java
@@ -0,0 +1,13 @@
+package br.com.tcc.prontuario.prontuario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MedSignupPersonalData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_med_signup_personal_data);
+    }
+}
