12c1be8fb36e7fb1006c67422f867ed151ae3e7a
VITOR MATHEUS REIS MARCELO <1510032667@acad.dvuadmin.net>
2018-03-29 19:38:19 -0300

    Inicio das telas de cadastro do pacinte

--- app/src/main/java/br/com/tcc/prontuario/prontuario/RegisterIdentificationActivity.java
+++ app/src/main/java/br/com/tcc/prontuario/prontuario/RegisterIdentificationActivity.java
@@ -1,18 +1,30 @@
 package br.com.tcc.prontuario.prontuario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class RegisterIdentificationActivity extends AppCompatActivity {
+
+    private void redirectToRegisterStep2(){
+        Intent registerStep2 = new Intent(RegisterIdentificationActivity.this, RegisterIdentificationStep2Activity.class);
+        startActivity(registerStep2);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_identification);
 
-        final Button buttonStep1 = findViewById(R.id.login_button);
+        final Button buttonStep1 = findViewById(R.id.register_button_step1);
 
-//        buttonStep1.setOnClickListener();
+        buttonStep1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectToRegisterStep2();
+            }
+        });
     }
 }
---
+++ app/src/main/java/br/com/tcc/prontuario/prontuario/RegisterIdentificationStep3Activity.java
@@ -0,0 +1,43 @@
+package br.com.tcc.prontuario.prontuario;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterIdentificationStep3Activity extends AppCompatActivity {
+
+    private void nextPage(){
+        Intent homeActivity = new Intent(RegisterIdentificationStep3Activity.this, HomeActivity.class);
+        startActivity(homeActivity);
+    }
+
+    private void backPage(){
+        Intent registerStep2 = new Intent(RegisterIdentificationStep3Activity.this, RegisterIdentificationStep2Activity.class);
+        startActivity(registerStep2);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_identification_step3);
+
+        final Button next = findViewById(R.id.register_next_button_step3);
+        final Button back = findViewById(R.id.register_back_button_step3);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextPage();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backPage();
+            }
+        });
+    }
+}
