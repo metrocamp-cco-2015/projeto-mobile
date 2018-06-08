1044b43707f5b49b8fcc022b7e023e6f40ea952a
ERIK RICARDO BERGANTIN BALTHAZAR <1510031171@acad.dvuadmin.net>
2018-06-08 19:10:39 -0300

    consultas de paciente

---
+++ app/src/main/java/br/com/tcc/prontuario/prontuario/ConsultsPacient.java
@@ -0,0 +1,15 @@
+package br.com.tcc.prontuario.prontuario;
+
+/**
+ * Created by 1510031171 on 08/06/2018.
+ */
+
+public class ConsultsPacient {
+    public Object[] consults;
+
+    public ConsultsPacient(Object[] consults) {
+        this.consults = consults;
+    }
+
+
+}
